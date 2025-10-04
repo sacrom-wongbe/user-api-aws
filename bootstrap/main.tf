b1ea548f25b625d6bcc97379761338d7d28fae75
sacrom-wongbe <wongbe04242004@gmail.com>
2025-10-04 01:34:15 -0700

    cicd test 4

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -75,7 +75,7 @@ resource "aws_iam_role" "github_actions_role" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/${var.github_branch}"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
           }
         }
       }
